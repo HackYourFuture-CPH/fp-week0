418eaafccea29c5b22c50533fb8a3df145549188
HackYourFuture DK <manisha270989@gmail.com>
2022-11-14 15:40:38 +0100

    created navabar compnent

---
+++ packages/client/src/components/Button/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+
+export default function Navbar() {
+  return (
+    <div>
+      <ul className="navbar-ul">
+        <li>Spices</li>
+        <li>About us</li>
+        <li>Contacts</li>
+        <li>Search Spices</li>
+      </ul>
+    </div>
+  );
+}
