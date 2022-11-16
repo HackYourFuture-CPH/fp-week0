11aa049006e9b04928f037c34fdb4295c8b949e9
HackYourFuture DK <manisha270989@gmail.com>
2022-11-16 12:32:56 +0100

    added some styles to navbar component

--- packages/client/src/components/Button/Navbar.js
+++ packages/client/src/components/Button/Navbar.js
@@ -3,13 +3,27 @@ import './Navbar.css';
 
 export default function Navbar() {
   return (
-    <div>
-      <ul className="navbar-ul">
-        <li>Spices</li>
-        <li>About us</li>
-        <li>Contacts</li>
-        <li>Search Spices</li>
-      </ul>
+    <div className="navbar">
+      <div className="top-nav-content">
+        <div className="top-nav">
+          <span>Simply Spices</span>
+          <ul>
+            <li>Sign in</li>
+            <li>Favorite</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+      <div className="bottom-nav-content">
+        <div className="bottom-nav">
+          <ul className="navbar-ul">
+            <li>Spices</li>
+            <li>About us</li>
+            <li>Contacts</li>
+            <li>Search Spices</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
