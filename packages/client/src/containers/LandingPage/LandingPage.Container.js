56a19d5a0882131486abe344a576fddd19df34d8
HackYourFuture DK <manisha270989@gmail.com>
2022-11-16 10:21:51 +0100

    added navbar compnent

--- packages/client/src/containers/LandingPage/LandingPage.Container.js
+++ packages/client/src/containers/LandingPage/LandingPage.Container.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './LandingPage.Style.css';
 import { ProductsList } from './ProductsList';
 import { products } from './Products';
+import Navbar from '../../components/Button/Navbar';
 
 export const LandingPage = () => {
   return (
     <div className="landing-page-container">
+      <Navbar />
       <div className="landing-page-header">
         <span>LANDING PAGE</span>
       </div>
